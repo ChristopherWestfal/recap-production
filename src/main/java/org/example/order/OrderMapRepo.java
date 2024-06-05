5c8916f482a822240302dfc90281069059765acd
ChrizDope <incubusid@googlemail.com>
2024-06-05 12:44:02 +0200

    Add ShopService

--- src/main/java/org/example/order/OrderMapRepo.java
+++ src/main/java/org/example/order/OrderMapRepo.java
@@ -3,11 +3,12 @@ package org.example.order;
 import org.example.interfaces.OrderRepo;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 public class OrderMapRepo implements OrderRepo {
 
-    Map<Integer,Order> orders = new HashMap<Integer,Order>();
+    Map<Integer,Order> orders = new HashMap<>();
     int counter = 0;
 
     @Override
@@ -18,23 +19,37 @@ public class OrderMapRepo implements OrderRepo {
 
     @Override
     public void removeOrder(Order order) {
-
-
-        counter--;
+        Iterator<Map.Entry<Integer, Order>> iterator = orders.entrySet().iterator();
+        while(iterator.hasNext()) {
+            Map.Entry<Integer, Order> entry = iterator.next();
+            if(entry.getValue().equals(order)) {
+                iterator.remove();
+                counter--;
+            }
+        }
     }
 
     @Override
     public Order getOrder(int index) {
-        return orders.get(index);
+        if (orders.containsKey(index))
+            return orders.get(index);
+        return null;
     }
 
     @Override
     public Order getOrder(String name) {
+        for (Order order : orders.values()) {
+            for(int i = 0; i < order.products().size(); i++) {
+                if(order.products().get(i).name().equals(name))
+                    return order;
+            }
+        }
         return null;
     }
 
     @Override
     public void getAll() {
-
+        if(orders.isEmpty()) System.out.println("No orders found");
+        else for(Order order : orders.values()){ System.out.println(orders.toString()); }
     }
 }
--- src/main/java/org/example/shop/ShopService.java
+++ src/main/java/org/example/shop/ShopService.java
@@ -1,19 +1,31 @@
 package org.example.shop;
 
+import org.example.interfaces.OrderRepo;
 import org.example.order.Order;
 import org.example.order.OrderListRepo;
+import org.example.order.OrderMapRepo;
 import org.example.product.Product;
 import org.example.product.ProductRepo;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ShopService {
-    OrderListRepo orderListRepo = new OrderListRepo();
+    OrderRepo orderRepo;
     ProductRepo productRepo = new ProductRepo();
 
+    public ShopService(OrderRepo orderRepo) {
+        this.orderRepo = orderRepo;
+
+        productRepo.add(new Product("Säge", "12", new BigInteger("49.99")));
+        productRepo.add(new Product("Holz", "15", new BigInteger("11.99")));
+        productRepo.add(new Product("Hammer", "13", new BigInteger("14.99")));
+        productRepo.add(new Product("Schrauben", "12", new BigInteger("0.99")));
+    }
+
     public void placeOrder(Order order) {
-        orderListRepo.addOrder(order);
+        orderRepo.addOrder(order);
     }
 
     public void checkProductExistence(String name){
